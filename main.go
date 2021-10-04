f32f606eaded4d938f86c16d7e3cb63f0f35eea0
tovinhtuan <tuan.tv100698@gmail.com>
2021-10-04 18:21:02 +0700

    fix

--- main.go
+++ main.go
@@ -15,39 +15,49 @@ type UserInformation struct {
 	Birthday string `json:"birthday"`
 }
 
+type FailedRequest struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+}
+
 func resInformationUser(w http.ResponseWriter, r *http.Request) {
 	user := UserInformation{"To Vinh Tuan", "tuantv", "male", "11/02/1998"}
 	userJson, err := json.Marshal(user)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(userJson)
 }
+
 func authenHeader(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
 		md := r.Header.Get("Authorization")
-		log.Println(md)
 		if md != "Bearer AKcqHRCTHaBLnznmH3fw6bRSMBSZpa9tAngkKnGydBmST5XFGpxzgsGMuT3z7QsZ" {
-			http.Error(w, "authentication error", http.StatusForbidden)
+			failReq := FailedRequest{false, "authentication error"}
+			fail, err := json.Marshal(failReq)
+			if err != nil {
+				log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+				return
+			}
+			// http.Error(w, "authentication error", http.StatusForbidden)
+			w.WriteHeader(http.StatusForbidden)
+			w.Write(fail)
 			return
 		}
 		next.ServeHTTP(w, r)
 	})
 }
+
 func main() {
 	port := "3333"
-
 	log.Printf("Starting up on http://localhost:%s", port)
-
 	r := chi.NewRouter()
 	r.Route("/api/me", func(r chi.Router) {
 		r.Use(authenHeader)
 		r.Get("/", resInformationUser)
-	})
-	r.Get("/test", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("adida phat"))
 	})
 	log.Fatal(http.ListenAndServe(":"+port, r))
 }
